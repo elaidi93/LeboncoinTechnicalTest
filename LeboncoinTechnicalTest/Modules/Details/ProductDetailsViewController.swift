aedbdd0b36e18c6335fd74a8b299e25679561ede
hamza <helaidi@exomind.fr>
2022-01-09 23:38:13 +0100

    [VC] Create products details ViewController

---
+++ LeboncoinTechnicalTest/Modules/Details/ProductDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProductDetailsViewController.swift
+//  LeboncoinTechnicalTest
+//
+//  Created by hamza on 09/01/2022.
+//
+
+import UIKit
+
+class ProductDetailsViewController: BaseViewController {
+    
+    var product: ProductResponse?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
