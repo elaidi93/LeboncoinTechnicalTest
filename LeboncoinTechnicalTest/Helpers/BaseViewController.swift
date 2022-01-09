82881cbe7b6bf946e6df949b6117d22d7bcac24c
hamza <helaidi@exomind.fr>
2022-01-09 21:44:53 +0100

    [Commun UI] use commun ViewController

---
+++ LeboncoinTechnicalTest/Helpers/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  LeboncoinTechnicalTest
+//
+//  Created by hamza on 09/01/2022.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemBackground
+    }
+
+}
