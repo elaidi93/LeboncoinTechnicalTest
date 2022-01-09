274b91f0427760acb57b119910b0192d4f456d1e
hamza <helaidi@exomind.fr>
2022-01-09 21:43:50 +0100

    [Strings] Add new file Constants

---
+++ LeboncoinTechnicalTest/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  LeboncoinTechnicalTest
+//
+//  Created by hamza on 09/01/2022.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let products_url = "https://raw.githubusercontent.com/leboncoin/paperclip/master/listing.json"
+    static let categories_url = "https://raw.githubusercontent.com/leboncoin/paperclip/master/categories.json"
+    
+}
