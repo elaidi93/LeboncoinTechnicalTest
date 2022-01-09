a117a6d5358de7024ea06ea1bc8c298d220679df
hamza <helaidi@exomind.fr>
2022-01-09 01:45:01 +0100

    Call homeViewController without using storyboard

--- LeboncoinTechnicalTest/HomeViewController.swift
+++ LeboncoinTechnicalTest/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  LeboncoinTechnicalTest
 //
 //  Created by hamza on 08/01/2022.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
