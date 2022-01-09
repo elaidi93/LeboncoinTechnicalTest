1672097ab86c626566d214d76f6cd150482b4bb4
hamza <helaidi@exomind.fr>
2022-01-09 21:45:44 +0100

    [Response] Create Product and Category responses using Codable

---
+++ LeboncoinTechnicalTest/Response/CategoryResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryResponse.swift
+//  LeboncoinTechnicalTest
+//
+//  Created by hamza on 09/01/2022.
+//
+
+import Foundation
+
+struct CategoryResponse: Codable {
+    let id: Int
+    let name: String
+}
