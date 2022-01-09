11ed74e1df1ff90aa039e9aabbbb6400b64531ee
hamza <helaidi@exomind.fr>
2022-01-09 21:47:56 +0100

    [UI] Create and fill tableview

--- LeboncoinTechnicalTest/HomeViewController.swift
+++ LeboncoinTechnicalTest/HomeViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import Combine
 
-class HomeViewController: UIViewController {
+class HomeViewController: BaseViewController {
+    
+    // MARK: - Variables:
+    private let tableView = UITableView()
+    private var observer: AnyCancellable?
+    private var products: [ProductResponse]? = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addSubview(tableView)
+        
+        // TableView Delegates
+        tableView.dataSource = self
+        
+        // fetch products
+        
+        products = ProductWorker.shared.fetchProducts()
+        observer = ProductWorker.shared.send.sink { products in
+            self.products = products
+        }
     }
 
-    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // TableView Layout
+        tableView.frame = self.view.safeAreaLayoutGuide.layoutFrame
+        
+    }
 
+}
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Cell: \(products?[indexPath.row].title ?? "")"
+        return cell
+    }
+    
 }
 
--- LeboncoinTechnicalTest/SceneDelegate.swift
+++ LeboncoinTechnicalTest/SceneDelegate.swift
@@ -18,12 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
+        // Use HomeViewController as RootViewController
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = HomeViewController()
         window?.makeKeyAndVisible()
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
