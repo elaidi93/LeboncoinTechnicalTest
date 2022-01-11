8ecae94a09b8770fb38fb42781644153433f8867
hamza <helaidi@exomind.fr>
2022-01-11 12:04:32 +0100

    [Cell] create custom product cell

--- LeboncoinTechnicalTest/Modules/Home/ProductTableViewCell.swift
+++ LeboncoinTechnicalTest/Modules/Home/ProductTableViewCell.swift
@@ -8,16 +8,67 @@
 import UIKit
 
 class ProductTableViewCell: UITableViewCell {
-
+    
+    private var imageLoader: ImageLoader?
+    
+    private var stackView: UIStackView {
+        let stackView = UIStackView()
+        stackView.spacing = 20
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.frame = self.contentView.frame
+        stackView.addArrangedSubview(image)
+        stackView.addArrangedSubview(infoLabel)
+        return stackView
+    }
+    private let image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+    
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        imageLoader = ImageLoader()
+        self.contentView.addSubview(stackView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        NSLayoutConstraint.activate([
+            image.heightAnchor.constraint(equalToConstant: 50),
+            image.widthAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configure(product: ProductViewModel) {
+        if let img = product.smallImage {
+            imageLoader?.load(from: img, completion: { imageData, error in
+                guard let imageData = imageData else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.image.image = UIImage(data: imageData)
+                }
+            })
+        }
+        
+        infoLabel.text = product.title
     }
 
 }
