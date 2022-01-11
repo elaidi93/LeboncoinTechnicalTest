da65a1b2f010c8cc8c337edcbac7bd5c24ee165c
hamza <helaidi@exomind.fr>
2022-01-11 19:14:45 +0100

    [ProductItem] Display details in product cell

--- LeboncoinTechnicalTest/Modules/Home/ProductTableViewCell.swift
+++ LeboncoinTechnicalTest/Modules/Home/ProductTableViewCell.swift
@@ -15,27 +15,56 @@ class ProductTableViewCell: UITableViewCell {
         let stackView = UIStackView()
         stackView.spacing = 20
         stackView.axis = .horizontal
-        stackView.distribution = .fillProportionally
+        stackView.distribution = .fill
+        stackView.alignment = .firstBaseline
         stackView.frame = self.contentView.frame
         stackView.addArrangedSubview(image)
-        stackView.addArrangedSubview(infoLabel)
+        stackView.addArrangedSubview(labelsStackView)
         return stackView
     }
+    
+    private var labelsStackView: UIStackView {
+        let stackView = UIStackView()
+        stackView.spacing = 8
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(categoryLabel)
+        stackView.addArrangedSubview(priceLabel)
+        return stackView
+    }
+    
     private let image: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-    private let infoLabel: UILabel = {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        return label
+    }()
+    
+    private let categoryLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
         return label
     }()
     
+    private let priceLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let formatter = NumberFormatter()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-    
+        formatter.numberStyle = .currency
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -51,12 +80,12 @@ class ProductTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         NSLayoutConstraint.activate([
-            image.heightAnchor.constraint(equalToConstant: 50),
-            image.widthAnchor.constraint(equalToConstant: 50)
+            image.heightAnchor.constraint(equalToConstant: 60),
+            image.widthAnchor.constraint(equalToConstant: 60)
         ])
     }
     
-    func configure(product: ProductViewModel) {
+    func configure(product: ProductViewModel, categories: [CategoryViewModel]?) {
         if let img = product.smallImage {
             imageLoader?.load(from: img, completion: { imageData, error in
                 guard let imageData = imageData else {
@@ -68,7 +97,16 @@ class ProductTableViewCell: UITableViewCell {
             })
         }
         
-        infoLabel.text = product.title
+        titleLabel.text = product.title
+        if let price = product.price,
+           let formattedPrice = formatter.string(from: NSNumber(value: price)) {
+            priceLabel.text = "Prix: \(formattedPrice)"
+        }
+        
+        if let category = categories?.first(where: { $0.id == product.categoryId })?.name {
+               categoryLabel.text = "Catégorie\(category)"
+           }
+        
     }
-
+    
 }
